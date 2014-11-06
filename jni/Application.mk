0356184c65713448a2b535df8140db6614f8afc0
Simo Kinnunen <simo@shoqolate.com>
2014-11-06 18:21:36 +0900

    Activate c++11.

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,9 @@
 APP_ABI := armeabi-v7a x86
 
+# Get C++11 working
+APP_CPPFLAGS += -std=c++11
+APP_STL := gnustl_static
+
 # Disable PIE for SDK <16 support. Enable manually for >=5.0
 # where necessary.
 APP_PIE := false
