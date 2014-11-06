e34c67b9b0a1cc80fecbdd96bedcef7143ebd94c
Simo Kinnunen <simo@shoqolate.com>
2014-11-06 17:55:38 +0900

    Detect need for -nopie automatically.

--- run.sh
+++ run.sh
@@ -3,6 +3,11 @@ set -exo pipefail
 ndk-build
 abi=$(adb shell getprop ro.product.cpu.abi | tr -d '\r')
 sdk=$(adb shell getprop ro.build.version.sdk | tr -d '\r')
-adb push libs/$abi/minicap /data/local/tmp/
+if (($sdk >= 16)); then
+  bin=minicap
+else
+  bin=minicap-nopie
+fi
+adb push libs/$abi/$bin /data/local/tmp/
 adb push jni/minicap-shared/aosp/libs/android-$sdk/$abi/minicap.so /data/local/tmp/
-adb shell LD_LIBRARY_PATH=/data/local/tmp /data/local/tmp/minicap "$@"
+adb shell LD_LIBRARY_PATH=/data/local/tmp /data/local/tmp/$bin "$@"
