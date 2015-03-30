1abe8864d0c035bf61fb958dfa4ab40b566c0ee7
Simo Kinnunen <simo@shoqolate.com>
2015-03-30 15:15:41 +0900

    Remove now unused vendor deps.

--- jni/vendor/libwebsockets/getdtablesize.c
+++
@@ -1,7 +0,0 @@
-#include <unistd.h>
-
-// This has apparently been deprecated and removed from the SDK, but
-// libwebsockets still uses it. Provide this instead.
-int getdtablesize(void) {
-  return sysconf(_SC_OPEN_MAX);
-}
