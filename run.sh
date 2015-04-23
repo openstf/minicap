e6b220d7c867d49af70e81da7d289d89de96807a
Simo Kinnunen <simo@shoqolate.com>
2015-04-23 23:17:22 +0900

    Add an option to have the run.sh script check the display's size and set it automatically. Makes checking easier.

--- run.sh
+++ run.sh
@@ -17,9 +17,23 @@ else
   bin=minicap-nopie
 fi
 
+args=
+if [ "$1" = "autosize" ]; then
+  set +o pipefail
+  size=$(adb shell dumpsys window | grep -Eo 'init=\d+x\d+' | head -1 | cut -d= -f 2)
+  if [ "$size" = "" ]; then
+    w=$(adb shell dumpsys window | grep -Eo 'DisplayWidth=\d+' | head -1 | cut -d= -f 2)
+    h=$(adb shell dumpsys window | grep -Eo 'DisplayHeight=\d+' | head -1 | cut -d= -f 2)
+    size="${w}x${h}"
+  fi
+  args="-P $size@$size/0"
+  set -o pipefail
+  shift
+fi
+
 # Create a directory for our resources
 dir=/data/local/tmp/minicap-devel
-adb shell mkdir -p $dir
+adb shell "mkdir $dir 2>/dev/null"
 
 # Upload the binary
 adb push libs/$abi/$bin $dir
@@ -28,7 +42,7 @@ adb push libs/$abi/$bin $dir
 adb push jni/minicap-shared/aosp/libs/android-$sdk/$abi/minicap.so $dir
 
 # Run!
-adb shell LD_LIBRARY_PATH=$dir $dir/$bin "$@"
+adb shell LD_LIBRARY_PATH=$dir $dir/$bin $args "$@"
 
 # Clean up
 adb shell rm -r $dir
