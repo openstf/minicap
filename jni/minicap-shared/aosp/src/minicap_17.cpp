74eacdc3082afde8e9d5385fbae5e206d9e9a1f2
Simo Kinnunen <simo@shoqolate.com>
2015-07-21 17:52:29 +0900

    Seems that lockNextBuffer() may EINTR. Repeat where possible. SA_RESTART doesn't seem to work well with the current code, causing occasional longer waits when killing. Could be a problem on our side, though.

--- jni/minicap-shared/aosp/src/minicap_17.cpp
+++ jni/minicap-shared/aosp/src/minicap_17.cpp
@@ -115,10 +115,16 @@ public:
   consumePendingFrame(Minicap::Frame* frame) {
     android::status_t err;
 
-    err = mConsumer->lockNextBuffer(&mBuffer);
+    do {
+      // This may call Fence::waitForever() behind the scenes, which calls
+      // ioctl(), which may EINTR. Seems to mostly happen on Samsung devices
+      // for some reason.
+      err = mConsumer->lockNextBuffer(&mBuffer);
+    }
+    while (err == -EINTR);
 
     if (err != android::NO_ERROR) {
-      MCERROR("Unable to lock next buffer %s", error_name(err));
+      MCERROR("Unable to lock next buffer %s (%d)", error_name(err), err);
       return false;
     }
 
