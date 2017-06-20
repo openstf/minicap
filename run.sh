3a5e7a617e4262839374d3852f24c22684f2a806
Marcin Jeleński <marcin@jelenski.net>
2017-06-20 22:30:26 +0200

    Regex fallback mode

--- run.sh
+++ run.sh
@@ -26,7 +26,15 @@ fi
 args=
 if [ "$1" = "autosize" ]; then
   set +o pipefail
-  size=$(adb shell dumpsys window | grep -Eo 'init=\d+x\d+' | head -1 | cut -d= -f 2)
+  size=$(adb shell dumpsys window | grep -Eo 'init=[0-9]+x[0-9]+' | head -1 | cut -d= -f 2)
+  if [ "$size" = "" ]; then
+    w=$(adb shell dumpsys window | grep -Eo 'DisplayWidth=[0-9]+' | head -1 | cut -d= -f 2)
+    h=$(adb shell dumpsys window | grep -Eo 'DisplayHeight=[0-9]+' | head -1 | cut -d= -f 2)
+    size="${w}x${h}"
+  fi
+  if [ "$size" = "" ]; then
+    size=$(adb shell dumpsys window | grep -Eo 'init=\d+x\d+' | head -1 | cut -d= -f 2)
+  fi
   if [ "$size" = "" ]; then
     w=$(adb shell dumpsys window | grep -Eo 'DisplayWidth=\d+' | head -1 | cut -d= -f 2)
     h=$(adb shell dumpsys window | grep -Eo 'DisplayHeight=\d+' | head -1 | cut -d= -f 2)
